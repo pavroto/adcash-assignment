559d65112286f54ef351566706701094325cb782
Pavel Rotov <paroto@ttu.ee>
2023-03-08 20:15:34 +0200

    Revert "general: settings.py: django-extensions added to INSTALLED_APPS"
    This reverts commit 0cc35e59ca3187a2a63d8f57e675f81d85182fae.

--- adcash_assignment/settings.py
+++ adcash_assignment/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'django_extensions'
     'uiapp.apps.UiappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
