05b410c2e62f46d3060aadbf0b47f6b1467d497a
Pavel Rotov <paroto@ttu.ee>
2023-03-09 04:06:16 +0200

    uiapp: guard.py: username_chaeck and name_check updated. NOT STABLE

--- uiapp/guard.py
+++ uiapp/guard.py
@@ -1,13 +1,6 @@
 import hashlib
 import re
 
-error_keys = {
-    1: "In name, only english letters are allowed",
-    2: 'Special characters (/, \\, ", \', -) are not allowed name and username',
-    3: "Passwords do not match",
-    4: "Password must be from 8 to 20 characters long"
-}
-
 
 def hashing(psw, psw_rp=""):
     psw = psw.encode() + b"6b5450e51be27571c15011ea41c53c1f"  # salting
@@ -26,57 +19,79 @@ def input_test(input_map, if_signin):
 
     def name_check(name):
         """
-        In First and Second names english letters are only allowed letters
+        In Name english letters are only allowed letters
         """
-        return 1
+
+        min_length = 2
+        max_length = 20
+        name = name.strip()
+        match = re.fullmatch("^[A-Za-z]*$", name)
+
+        if len(name) > max_length:
+            return "Name is too long", 1
+        elif len(name) < min_length:
+            return "Name is too short", 1
+        elif not match:
+            return "In name, only english letters are allowed", 1
+        else:
+            return name, 0
 
     def username_check(username):
         """
         Special characters are restricted
         """
-        return 1
+        min_length = 5
+        max_length = 20
+        username = username.strip()
+        restricted_chars = ['/', '\\', '\'', '"']
 
-    def psw_check(psw, psw_rp):
+        for char in username:
+            for i in restricted_chars:
+                if char == i:
+                    return "Special characters('/', '\\', '\'', '\"') are restricted", 1
+
+        if len(username) > max_length or len(username) < min_length:
+            return format(f"Username must be from {min_length} to {max_length} characters long"), 1
+
+        return username, 0
+
+    def psw_check(psw):
         """
         Check that passwords follow next rules:
             - Are 8-20 characters long
-            - Match
-
         Then the password is hashed and returned as hex hash
         """
-        return 2
+        return "psw: debug error", 1
 
     if if_signin:
-        # input_map = {
-        #             "first_name": "Hello1",
-        #             "second_name": "Hello2",
-        #             "username": "Hello3",
-        #             "psw": "Hello4",
-        #             "psw_rp": "Hello5"
-        #         }
-        input_map['first_name'] = name_check(input_map['first_name'])
-        input_map['second_name'] = name_check(input_map['second_name'])
-        input_map['username'] = username_check(input_map['username'])
-
-        input_map['psw'] = psw_check(input_map['psw'], input_map['psw_rp'])
-        input_map['psw_rp'] = ''
-
+        """
+        Explanation of next For loop:
+        
+        
+        """
         error_list = []
         for key, value in input_map.items():
-            for i in error_keys:
-                if value == i:
-                    error_list.append(i)
+            if key == 'first_name' or key == 'second_name':
+                function_output, error = name_check(input_map[key])
+            elif key == 'username':
+                function_output, error = username_check(input_map[key])
+            elif key == 'psw' or key == 'psw_rp':
+                function_output, error = psw_check(input_map[key])
+            else:
+                return [None, None]
+
+            if error:
+                error_list.append(function_output)
+                input_map[key] = ''
+                continue
+            input_map[key] = function_output
 
         if len(error_list) != 0:
-            error_list = [*set(error_list)]  # Remove duplicate errors just in case
-
-            for i in range(len(error_list)):
-                error_list[i] = error_keys[error_list[i]]
-
-            return error_list
+            error_list = [*set(error_list)]
+            return [input_map, error_list]
 
         else:
-            return input_map
+            return [input_map, None]
 
     else:
         return {}
