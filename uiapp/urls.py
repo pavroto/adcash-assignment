dcadbffffd5b2b69bcdccd6797581740d5a247df
Pavel Rotov <paroto@ttu.ee>
2023-03-11 00:39:45 +0200

    uiapp: views.py: logout_view added

--- uiapp/urls.py
+++ uiapp/urls.py
@@ -6,5 +6,6 @@ app_name = 'uiapp'
 urlpatterns = [
     path('', views.index, name="index"),
     path('signin/', views.signin, name="signin"),
-    path('register/', views.register, name="register")
+    path('register/', views.register, name="register"),
+    path('logout/', views.logout_view, name="logout")
 ]
