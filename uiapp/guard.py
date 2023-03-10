f0d43ac07edba474307c13a73c5d9545a4b3daae
Pavel Rotov <paroto@ttu.ee>
2023-03-10 20:46:53 +0200

    uiapp: guard.py: refactored case check, added password check, updated other checks

--- uiapp/guard.py
+++ uiapp/guard.py
@@ -1,8 +1,17 @@
 import hashlib
 import re
 
+NAME_MIN_LENGTH = 2
+NAME_MAX_LENGTH = 20
 
-def hashing(psw, psw_rp=""):
+USERNAME_MIN_LENGTH = 5
+USERNAME_MAX_LENGTH = 20
+
+PASSWORD_MIN_LENGTH = 8
+PASSWORD_MAX_LENGTH = 20
+
+
+def hashing(psw):
     psw = psw.encode() + b"6b5450e51be27571c15011ea41c53c1f"  # salting
     hashed_psw = hashlib.sha512()
     hashed_psw.update(psw)
@@ -10,27 +19,17 @@ def hashing(psw, psw_rp=""):
     return hashed_psw.hexdigest()
 
 
-def input_test(input_map, if_signin):
-    """
-    if_signin variable tells the function if it is received SIGN IN or REGISTER input maps
-    True -> SIGN IN
-    False -> REGISTER
-    """
-
+def input_test(input_map, case):
     def name_check(name):
         """
-        In Name english letters are only allowed letters
+        In Name english letters are only allowed characters
         """
 
-        min_length = 2
-        max_length = 20
         name = name.strip()
         match = re.fullmatch("^[A-Za-z]*$", name)
 
-        if len(name) > max_length:
-            return "Name is too long", 1
-        elif len(name) < min_length:
-            return "Name is too short", 1
+        if len(name) < NAME_MIN_LENGTH or len(name) > NAME_MAX_LENGTH:
+            return f"Name must be from {NAME_MIN_LENGTH} to {NAME_MAX_LENGTH} characters long each", 1
         elif not match:
             return "In name, only english letters are allowed", 1
         else:
@@ -40,8 +39,7 @@ def input_test(input_map, if_signin):
         """
         Special characters are restricted
         """
-        min_length = 5
-        max_length = 20
+
         username = username.strip()
         restricted_chars = ['/', '\\', '\'', '"']
 
@@ -50,26 +48,28 @@ def input_test(input_map, if_signin):
                 if char == i:
                     return "Special characters('/', '\\', '\'', '\"') are restricted", 1
 
-        if len(username) > max_length or len(username) < min_length:
-            return format(f"Username must be from {min_length} to {max_length} characters long"), 1
+        if len(username) < USERNAME_MIN_LENGTH or len(username) > USERNAME_MAX_LENGTH:
+            return format(f"Username must be from {USERNAME_MIN_LENGTH} to {USERNAME_MAX_LENGTH} characters long"), 1
 
         return username, 0
 
     def psw_check(psw):
         """
         Check that passwords follow next rules:
-            - Are 8-20 characters long
-        Then the password is hashed and returned as hex hash
+            - Are PASSWORD_MIN_LENGTH - PASSWORD_MAX_LENGTH characters long
+        Then the password is hashed and returned as a hex hash
         """
-        return "psw: debug error", 1
 
-    if if_signin:
-        """
-        Explanation of next For loop:
-        
-        
-        """
+        if len(psw) < PASSWORD_MIN_LENGTH or len(psw) > PASSWORD_MAX_LENGTH:
+            return f"Password must be from {PASSWORD_MIN_LENGTH} to {PASSWORD_MAX_LENGTH} characters", 1
+        else:
+            return hashing(psw), 0
+
+    if case == 'LOGIN':
+        return {None, None}
+    elif case == 'REGISTER':
         error_list = []
+
         for key, value in input_map.items():
             if key == 'first_name' or key == 'second_name':
                 function_output, error = name_check(input_map[key])
@@ -86,12 +86,15 @@ def input_test(input_map, if_signin):
                 continue
             input_map[key] = function_output
 
+        if input_map['psw'] != input_map['psw_rp']:
+            error_list.append('Passwords do not match')
+
         if len(error_list) != 0:
             error_list = [*set(error_list)]
-            return [input_map, error_list]
+            return [None, error_list]
 
         else:
             return [input_map, None]
 
     else:
-        return {}
+        return [None, None]
