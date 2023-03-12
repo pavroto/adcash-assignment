46a026295058fd39e7f7c02c3dbd91b45bcc8f5e
Pavel Rotov <paroto@ttu.ee>
2023-03-12 19:51:05 +0200

    apiapp: views.py added  ListLoansView

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,11 +1,25 @@
 from django.http import JsonResponse
 from uiapp.models import Loan
 from .serializers import ListLoansSerializer
+from django.contrib.auth.models import User
 
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
 
 def list_loans(request):
     loans = Loan.objects.all()
     serializer = ListLoansSerializer(loans, many=True)
     return JsonResponse({'loans': serializer.data})
+
+
+class ListLoansView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        if request.user:
+            loans = Loan.objects.filter(user=request.user).all()
+            serializer = ListLoansSerializer(loans, many=True)
+            return JsonResponse({'loans': serializer.data})
