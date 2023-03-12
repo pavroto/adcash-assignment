1ac8213b120121b92bcd500d279fc7eaa880106d
Pavel Rotov <paroto@ttu.ee>
2023-03-12 16:15:44 +0200

    uiapp: models.py: added functions to return data for Loan model

--- uiapp/models.py
+++ uiapp/models.py
@@ -11,6 +11,24 @@ class Loan(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     term = models.DateField()
 
+    def get_amount(self):
+        return self.amount
+
+    def get_debt(self):
+        return self.debt
+
+    def get_term(self):
+        return self.term
+
+    def get_full_name(self):
+        return self.user.get_full_name()
+
+    def get_username(self):
+        return self.user.get_username()
+
+    def get_timestamp(self):
+        return self.timestamp
+
 
 class BlackList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
