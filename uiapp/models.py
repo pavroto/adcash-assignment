13b5fc7f67fde3bcd83362f803a47ab3e9770156
Pavel Rotov <paroto@ttu.ee>
2023-03-12 04:09:20 +0200

    uiapp: models.py: Loan and BlackList models declared

--- uiapp/models.py
+++ uiapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Loan(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    debt = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    term = models.DateField()
+
+
+class BlackList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
