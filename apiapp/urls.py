2f0c8f2943f69ba3b9fb4ec53656c09354717560
Pavel Rotov <paroto@ttu.ee>
2023-03-12 21:00:34 +0200

    apiapp: urls.py added path for apply

--- apiapp/urls.py
+++ apiapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'apiapp'
 urlpatterns = [
     path('', views.ListLoansView.as_view(), name="ListLoansView"),
+    path('apply/', views.ApplyLoansView.as_view(), name="ApplyLoansView")
 ]
